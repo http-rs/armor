13849eb9518f6aedf1faf1504b9cbd843069af52
cak <cak@fastmail.com>
2019-10-11 06:04:43 -0400

    Update CSP test

--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,4 @@
+use armor::csp;
 use std::error::Error;
 
 #[test]
@@ -7,18 +8,18 @@ fn should_work() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
 
 #[test]
 fn csp_test() {
+    let mut policy = armor::csp::new();
+    policy
+        .default_src(csp::Source::SameOrigin)
+        .default_src("areweasyncyet.rs")
+        .script_src(csp::Source::SameOrigin)
+        .script_src(csp::Source::UnsafeInline)
+        .object_src(csp::Source::None)
+        .base_uri(csp::Source::None)
+        .upgrade_insecure_requests();
     let mut headers = http::HeaderMap::new();
     armor::armor(&mut headers);
-    let mut csp_policy = armor::ContentSecurityPolicy::new();
+    policy.apply(&mut headers);
 
-    csp_policy
-        .default_src(&["'self'", "areweasyncyet.rs"])
-        .script_src(&["'self'", "'unsafe-inline'"])
-        .object_src(&["'none'"])
-        .base_uri(&["'none'"])
-        .upgrade_insecure_requests();
-
-    armor::content_security_policy(&mut headers, csp_policy);
-
-    assert_eq!(headers["content-security-policy"], "default-src 'self' areweasyncyet.rs; script-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'none'; upgrade-insecure-requests");
+    assert_eq!(headers["content-security-policy"], "base-uri 'none'; default-src 'self' areweasyncyet.rs; object-src 'none'; script-src 'self' 'unsafe-inline'; upgrade-insecure-requests");
 }
