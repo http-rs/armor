68ac07f033457998fe61320971f39c5400a311f8
cak <cak@fastmail.com>
2019-10-11 06:03:34 -0400

    Update CSP example

--- examples/content_security_policy.rs
+++ examples/content_security_policy.rs
@@ -1,18 +1,18 @@
+use armor::csp;
+
 fn main() {
+    let mut policy = armor::csp::new();
+    policy
+        .default_src(csp::Source::SameOrigin)
+        .default_src("areweasyncyet.rs")
+        .script_src(csp::Source::SameOrigin)
+        .script_src(csp::Source::UnsafeInline)
+        .object_src(csp::Source::None)
+        .base_uri(csp::Source::None)
+        .upgrade_insecure_requests();
     let mut headers = http::HeaderMap::new();
     armor::armor(&mut headers);
-    let mut csp_policy = armor::ContentSecurityPolicy::new();
+    policy.apply(&mut headers);
 
-    csp_policy
-        .default_src(&["'self'", "areweasyncyet.rs"])
-        .script_src(&["'self'"])
-        .object_src(&["'none'"])
-        .upgrade_insecure_requests();
-
-    armor::content_security_policy(&mut headers, csp_policy); // "content-security-policy": "default-src 'self' areweasyncyet.rs; script-src 'self'; object-src 'none'; upgrade-insecure-requests"}
-
-    assert_eq!(headers["content-security-policy"], "default-src 'self' areweasyncyet.rs; script-src 'self'; object-src 'none'; upgrade-insecure-requests");
-
-    println!("{:?}", headers)
-    // {"x-dns-prefetch-control": "on", "x-content-type-options": "nosniff", "x-frame-options": "sameorigin", "strict-transport-security": "max-age=5184000", "x-xss-protection": "1; mode=block", "content-security-policy": "default-src 'self' areweasyncyet.rs; script-src 'self'; object-src 'none'; upgrade-insecure-requests"}
+    assert_eq!(headers["content-security-policy"], "base-uri 'none'; default-src 'self' areweasyncyet.rs; object-src 'none'; script-src 'self' 'unsafe-inline'; upgrade-insecure-requests");
 }
